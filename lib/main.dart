92c584af20f1c999aacafe943742f508d963b4ce
Mahipal <mshekhawat1995@gmail.com>
2023-05-12 18:56:05 +0530

    new commit 121212

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times and added new ci cd:',
+              'You have pushed the button this many times and added new ci cd: new',
             ),
             Text(
               '$_counter',
